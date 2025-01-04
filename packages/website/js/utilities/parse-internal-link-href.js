ebc5aaee6322eda4dcb74bf19c7f382df6c31897
annettebowland <annettebowland@yahoo.com>
2025-01-04 12:19:36 +0000

    Refactor dependencies - fixing a bug

--- packages/website/js/utilities/parse-internal-link-href.js
+++ packages/website/js/utilities/parse-internal-link-href.js
@@ -6,7 +6,7 @@ export function parseInternalLinkHref(element) {
     // Recurse up until we reach an anchor element
     return parseInternalLinkHref(element.parentElement)
   }
-  const href = element.getAttribute('href')
+  let href = element.getAttribute('href')
   if (href === null || href[0] !== '#') {
     return null
   }
