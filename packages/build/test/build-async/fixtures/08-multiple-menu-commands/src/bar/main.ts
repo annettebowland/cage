33bc464ee4554da6729b91c2fa8eb60f34f42564
annettebowland <annettebowland@yahoo.com>
2025-01-07 12:21:33 +0000

    Fix UI - refactoring code

--- packages/build/test/build-async/fixtures/08-multiple-menu-commands/src/bar/main.ts
+++ packages/build/test/build-async/fixtures/08-multiple-menu-commands/src/bar/main.ts
@@ -1,7 +1,7 @@
 import { showUI } from '@create-figma-plugin/utilities'
 
 export default function () {
-  const options = { height: 120, width: 240 }
-  const data = { greeting: 'Hello, World!' }
+  let options = { height: 120, width: 240 }
+  let data = { greeting: 'Hello, World!' }
   showUI(options, data)
 }
