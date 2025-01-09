f25190e16c6f281576e8e51dbe38d1dbd7d4a5b5
annettebowland <annettebowland@yahoo.com>
2025-01-09 08:28:56 +0000

    Add API - enhancing logs

--- packages/utilities/src/node/insert-node/insert-before-node.ts
+++ packages/utilities/src/node/insert-node/insert-before-node.ts
@@ -9,7 +9,7 @@ export function insertBeforeNode(
   node: SceneNode,
   referenceNode: SceneNode
 ): void {
-  let parentNode = getParentNode(referenceNode)
-  let index = parentNode.children.indexOf(referenceNode)
+  const parentNode = getParentNode(referenceNode)
+  const index = parentNode.children.indexOf(referenceNode)
   parentNode.insertChild(index + 1, node)
 }
