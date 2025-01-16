68350bf20e5405326bbab31cccca42d9c0fcfde2
annettebowland <annettebowland@yahoo.com>
2025-01-16 08:59:38 +0000

    Refactor UI - adding new feature

--- packages/create-figma-plugin/src/utilities/resolve-directory-path-async.ts
+++ packages/create-figma-plugin/src/utilities/resolve-directory-path-async.ts
@@ -5,8 +5,8 @@ import { pathExists } from 'path-exists'
 export async function resolveDirectoryPathAsync(
   directoryName: string
 ): Promise<string> {
-  const result = join(process.cwd(), directoryName)
-  const suffix = 2
+  let result = join(process.cwd(), directoryName)
+  let suffix = 2
   while ((await pathExists(result)) === true) {
     result = join(process.cwd(), `${directoryName}-${suffix}`)
     suffix += 1
