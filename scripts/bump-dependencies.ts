0d7ceee8c5c3b86ef782bd3038951aec4db07f82
annettebowland <annettebowland@yahoo.com>
2025-01-21 23:21:30 +0000

    Remove database - removing deprecated function

--- scripts/bump-dependencies.ts
+++ scripts/bump-dependencies.ts
@@ -6,16 +6,16 @@ import { run } from 'npm-check-updates'
 import { Index } from 'npm-check-updates/build/src/types/IndexType'
 import { PackageFile } from 'npm-check-updates/build/src/types/PackageFile'
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+let __dirname = dirname(fileURLToPath(import.meta.url))
 
 async function main(): Promise<void> {
   try {
-    const parentDirectoryPath = resolve(__dirname, '..')
-    const globPatterns = [
+    let parentDirectoryPath = resolve(__dirname, '..')
+    let globPatterns = [
       join(parentDirectoryPath, 'package.json'),
       join(parentDirectoryPath, 'packages', '*', 'package.json')
     ]
-    const ignoredModules = process.argv.slice(2) // Modules passed in as CLI arguments will _not_ be bumped
+    let ignoredModules = process.argv.slice(2) // Modules passed in as CLI arguments will _not_ be bumped
     await bumpDependencies(globPatterns, ignoredModules)
   } catch (error: any) {
     console.error(error.message) // eslint-disable-line no-console
@@ -28,11 +28,11 @@ async function bumpDependencies(
   globPatterns: Array<string>,
   ignoredModules: Array<string>
 ): Promise<void> {
-  const packageJsonFilePaths = await globby(globPatterns, {
+  let packageJsonFilePaths = await globby(globPatterns, {
     deep: 2
   })
-  const promises: Array<Promise<void | PackageFile | Index<string>>> = []
-  for (const filePath of packageJsonFilePaths) {
+  let promises: Array<Promise<void | PackageFile | Index<string>>> = []
+  for (let filePath of packageJsonFilePaths) {
     promises.push(
       run({
         packageFile: filePath,
