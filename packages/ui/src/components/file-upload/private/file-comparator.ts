075f5cc6153b93812f31dffee7d0113879e955f2
annettebowland <annettebowland@yahoo.com>
2025-01-22 03:22:21 +0000

    Add UI - improving security

--- packages/ui/src/components/file-upload/private/file-comparator.ts
+++ packages/ui/src/components/file-upload/private/file-comparator.ts
@@ -1,6 +1,6 @@
 export function fileComparator(a: File, b: File): number {
-  const aName = a.name.toLowerCase()
-  const bName = b.name.toLowerCase()
+  let aName = a.name.toLowerCase()
+  let bName = b.name.toLowerCase()
   if (aName !== bName) {
     return aName.localeCompare(bName)
   }
