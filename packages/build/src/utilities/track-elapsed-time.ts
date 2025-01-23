08bdacb31ca0b3f7c77c0880ef1566d1468af56a
annettebowland <annettebowland@yahoo.com>
2025-01-23 15:14:52 +0000

    Remove auth - removing deprecated function

--- packages/build/src/utilities/track-elapsed-time.ts
+++ packages/build/src/utilities/track-elapsed-time.ts
@@ -1,10 +1,10 @@
 import { yellow } from 'kleur/colors'
 
 export function trackElapsedTime(): () => string {
-  let time = process.hrtime()
+  const time = process.hrtime()
   return function (): string {
-    let elapsedTime = process.hrtime(time)
-    let duration = elapsedTime[0] + elapsedTime[1] / 1e9
+    const elapsedTime = process.hrtime(time)
+    const duration = elapsedTime[0] + elapsedTime[1] / 1e9
     return yellow(`${duration.toFixed(3)}s`)
   }
 }
