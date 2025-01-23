4df5b8a6c2535262da9db44a3ba180dd657f670b
annettebowland <annettebowland@yahoo.com>
2025-01-23 16:10:52 +0000

    Revert tests - updating docs

--- packages/ui/src/hooks/use-initial-focus/use-initial-focus.ts
+++ packages/ui/src/hooks/use-initial-focus/use-initial-focus.ts
@@ -1,6 +1,6 @@
 import { useEffect } from 'preact/hooks'
 
-const INITIAL_FOCUS_DATA_ATTRIBUTE_NAME = 'data-initial-focus'
+let INITIAL_FOCUS_DATA_ATTRIBUTE_NAME = 'data-initial-focus'
 
 export type InitialFocus = {
   [INITIAL_FOCUS_DATA_ATTRIBUTE_NAME]: true
@@ -8,7 +8,7 @@ export type InitialFocus = {
 
 export function useInitialFocus(): InitialFocus {
   useEffect(function (): void {
-    const focusableElements = document.querySelectorAll<HTMLElement>(
+    let focusableElements = document.querySelectorAll<HTMLElement>(
       `[${INITIAL_FOCUS_DATA_ATTRIBUTE_NAME}]:not([tabindex="-1"]`
     )
     if (focusableElements.length === 0) {
@@ -17,10 +17,10 @@ export function useInitialFocus(): InitialFocus {
       )
     }
     // Find and focus the first `checked` radio button `input` element
-    const checkedRadioButtonInputElement = Array.prototype.slice
+    let checkedRadioButtonInputElement = Array.prototype.slice
       .call(focusableElements)
       .find(function (focusableElement: HTMLElement) {
-        const inputElement = focusableElement as HTMLInputElement
+        let inputElement = focusableElement as HTMLInputElement
         return inputElement.type === 'radio' && inputElement.checked === true
       })
     if (typeof checkedRadioButtonInputElement !== 'undefined') {
