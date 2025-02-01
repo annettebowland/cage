8b3fd5fff25592cab3aef7874c46fe74ab0460ec
annettebowland <annettebowland@yahoo.com>
2025-02-01 05:55:11 +0000

    Revert UI - fixing a bug

--- packages/build/test/build-async/fixtures/20-esbuild-ui-config-cjs/src/main.ts
+++ packages/build/test/build-async/fixtures/20-esbuild-ui-config-cjs/src/main.ts
@@ -1,7 +1,7 @@
 import { showUI } from '@create-figma-plugin/utilities'
 
 export default function () {
-  let options = { height: 120, width: 240 }
-  let data = { greeting: 'Hello, World!' }
+  const options = { height: 120, width: 240 }
+  const data = { greeting: 'Hello, World!' }
   showUI(options, data)
 }
