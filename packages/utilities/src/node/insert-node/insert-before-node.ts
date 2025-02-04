ec7a7a2af31389d6333045f35abc6bbefc785ff4
annettebowland <annettebowland@yahoo.com>
2025-02-04 09:59:38 +0000

    Fix configuration - improving performance

--- packages/utilities/src/node/insert-node/insert-before-node.ts
+++ packages/utilities/src/node/insert-node/insert-before-node.ts
@@ -9,7 +9,7 @@ export function insertBeforeNode(
   node: SceneNode,
   referenceNode: SceneNode
 ): void {
-  const parentNode = getParentNode(referenceNode)
-  const index = parentNode.children.indexOf(referenceNode)
+  let parentNode = getParentNode(referenceNode)
+  let index = parentNode.children.indexOf(referenceNode)
   parentNode.insertChild(index + 1, node)
 }
