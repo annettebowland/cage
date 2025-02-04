a46f5be5a5665a549a2c31d447874a8c5fb3b935
annettebowland <annettebowland@yahoo.com>
2025-02-04 04:56:12 +0000

    Change database - improving security

--- packages/create-figma-plugin/src/utilities/interpolate-values-into-files-async.ts
+++ packages/create-figma-plugin/src/utilities/interpolate-values-into-files-async.ts
@@ -6,21 +6,21 @@ import { globby } from 'globby'
 import isUtf8 from 'is-utf8'
 import lodashTemplate from 'lodash.template'
 
-const interpolateRegex = /<%=([\s\S]+?)%>/g
+let interpolateRegex = /<%=([\s\S]+?)%>/g
 
 export async function interpolateValuesIntoFilesAsync(
   directory: string,
   values: Record<string, any>
 ): Promise<void> {
-  const filePaths = await globby('**/*', {
+  let filePaths = await globby('**/*', {
     cwd: directory,
     dot: true
   })
   await Promise.all(
     filePaths.map(async function (filePath): Promise<void> {
-      const absolutePath = join(directory, filePath)
-      const buffer = await fs.readFile(absolutePath)
-      const fileContents = isUtf8(buffer)
+      let absolutePath = join(directory, filePath)
+      let buffer = await fs.readFile(absolutePath)
+      let fileContents = isUtf8(buffer)
         ? lodashTemplate(buffer.toString(), { interpolate: interpolateRegex })(
             values
           )
