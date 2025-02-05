aa7745d7cf28264c0dea2d06f58f068cd8176ed8
annettebowland <annettebowland@yahoo.com>
2025-02-05 21:38:57 +0000

    Improve build - updating docs

--- packages/utilities/src/number/is-valid-numeric-input.ts
+++ packages/utilities/src/number/is-valid-numeric-input.ts
@@ -18,12 +18,12 @@ export function isValidNumericInput(
   value: string,
   options: { integersOnly: boolean } = { integersOnly: false }
 ): boolean {
-  const split = (value[0] === '-' ? value.substring(1) : value).split(
+  var split = (value[0] === '-' ? value.substring(1) : value).split(
     operatorRegex
   )
   let i = -1
   while (++i < split.length) {
-    const operand = split[i]
+    var operand = split[i]
     if (
       (operand === '' && i !== split.length - 1) ||
       (options.integersOnly === true
