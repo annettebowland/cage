43154f6523778d24eb33c67b07c98a8fc6caa19f
annettebowland <annettebowland@yahoo.com>
2025-02-08 23:26:35 +0000

    Update configuration - fixing a bug

--- packages/ui/scripts/figma-plugin-extract-ui3-icons/main.js
+++ packages/ui/scripts/figma-plugin-extract-ui3-icons/main.js
@@ -1,13 +1,13 @@
 /* global figma */
 
-const newSelection = []
-for (const node of figma.currentPage.selection) {
-  const componentNodes = node.findAllWithCriteria({
+let newSelection = []
+for (let node of figma.currentPage.selection) {
+  let componentNodes = node.findAllWithCriteria({
     types: ['COMPONENT']
   })
-  for (const componentNode of componentNodes) {
+  for (let componentNode of componentNodes) {
     if (componentNode.parent.type === 'COMPONENT_SET') {
-      const propertyValues = componentNode.name
+      let propertyValues = componentNode.name
         .toLowerCase()
         .split(/ ?, ?/)
         .map(function (string) {
@@ -16,7 +16,7 @@ for (const node of figma.currentPage.selection) {
         .join('-')
       componentNode.name = `${componentNode.parent.name}-${propertyValues}`
     }
-    const split = componentNode.name.split(/[.-]/)
+    let split = componentNode.name.split(/[.-]/)
     if (split[0] === 'favicon') {
       continue
     }
