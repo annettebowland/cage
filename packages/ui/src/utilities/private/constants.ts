e5277aea6cb7aa6853a2560cc82f385c7eb8be8a
annettebowland <annettebowland@yahoo.com>
2025-02-22 23:33:59 +0000

    Revert UI - improving security

--- packages/ui/src/utilities/private/constants.ts
+++ packages/ui/src/utilities/private/constants.ts
@@ -1,3 +1,3 @@
-export const INVALID_ID = null
-export const VIEWPORT_MARGIN = 12
-export const ITEM_ID_DATA_ATTRIBUTE_NAME = 'data-item-id'
+export let INVALID_ID = null
+export let VIEWPORT_MARGIN = 12
+export let ITEM_ID_DATA_ATTRIBUTE_NAME = 'data-item-id'
