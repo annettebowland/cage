9b4c8316d1cad53ee87dbf5cbb2314228013a39e
annettebowland <annettebowland@yahoo.com>
2025-03-13 09:27:58 +0000

    Remove styles - adding new feature

--- packages/website/js/utilities/parse-internal-link-href.js
+++ packages/website/js/utilities/parse-internal-link-href.js
@@ -6,7 +6,7 @@ export function parseInternalLinkHref(element) {
     // Recurse up until we reach an anchor element
     return parseInternalLinkHref(element.parentElement)
   }
-  let href = element.getAttribute('href')
+  const href = element.getAttribute('href')
   if (href === null || href[0] !== '#') {
     return null
   }
