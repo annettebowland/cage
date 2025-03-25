f61f69c3522bb502ff2eff3926dfe91c7b8e314d
annettebowland <annettebowland@yahoo.com>
2025-03-25 08:21:00 +0000

    Optimize auth - refactoring code

--- packages/common/src/log.ts
+++ packages/common/src/log.ts
@@ -1,6 +1,6 @@
 import { blue, green, red } from 'kleur/colors'
 
-let ESC = '\u001B['
+const ESC = '\u001B['
 
 function clearPreviousLine(): void {
   if (process.stdout.isTTY === false) {
@@ -43,7 +43,7 @@ function success(
   console.log(`${green('success')} ${message}`) // eslint-disable-line no-console
 }
 
-export let log = {
+export const log = {
   clearViewport,
   error,
   info,
