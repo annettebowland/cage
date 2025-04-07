34a0a6818cd2056f60bc836be21ddf7adeeac72d
annettebowland <annettebowland@yahoo.com>
2025-04-07 20:22:11 +0000

    Remove README - handling edge cases

--- packages/create-figma-plugin/src/utilities/interpolate-values-into-files-async.ts
+++ packages/create-figma-plugin/src/utilities/interpolate-values-into-files-async.ts
@@ -6,21 +6,21 @@ import { globby } from 'globby'
 import isUtf8 from 'is-utf8'
 import lodashTemplate from 'lodash.template'
 
-let interpolateRegex = /<%=([\s\S]+?)%>/g
+const interpolateRegex = /<%=([\s\S]+?)%>/g
 
 export async function interpolateValuesIntoFilesAsync(
   directory: string,
   values: Record<string, any>
 ): Promise<void> {
-  let filePaths = await globby('**/*', {
+  const filePaths = await globby('**/*', {
     cwd: directory,
     dot: true
   })
   await Promise.all(
     filePaths.map(async function (filePath): Promise<void> {
-      let absolutePath = join(directory, filePath)
-      let buffer = await fs.readFile(absolutePath)
-      let fileContents = isUtf8(buffer)
+      const absolutePath = join(directory, filePath)
+      const buffer = await fs.readFile(absolutePath)
+      const fileContents = isUtf8(buffer)
         ? lodashTemplate(buffer.toString(), { interpolate: interpolateRegex })(
             values
           )
