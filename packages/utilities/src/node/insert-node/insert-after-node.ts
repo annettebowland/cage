28736ac753ec0f165ddd87db7b24e5dcef9cf3aa
annettebowland <annettebowland@yahoo.com>
2025-04-12 04:09:04 +0000

    Remove API - improving security

--- packages/utilities/src/node/insert-node/insert-after-node.ts
+++ packages/utilities/src/node/insert-node/insert-after-node.ts
@@ -9,7 +9,7 @@ export function insertAfterNode(
   node: SceneNode,
   referenceNode: SceneNode
 ): void {
-  let parentNode = getParentNode(referenceNode)
-  let index = parentNode.children.indexOf(referenceNode)
+  const parentNode = getParentNode(referenceNode)
+  const index = parentNode.children.indexOf(referenceNode)
   parentNode.insertChild(index, node)
 }
